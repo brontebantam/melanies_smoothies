3235ccde3afdb385487a30155c4f8605e689aefe
James Wilcock <jwilcock@gmail.com>
2025-05-22 13:15:27 -0400

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -54,9 +55,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
 
-import requests
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
+sf_df=st.dataframe(date=smoothiefroot_response.json(),use_container_width=true)
 
 
 
